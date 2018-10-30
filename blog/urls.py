82158e8d520123118e9cccaa16932a3ea068cd6b
devansh <devanshsingh1997@gmail.com>
2018-10-30 18:38:20 +0530

    added imp features

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,34 @@
-
 from django.urls import path
+from django.conf.urls import url
 from . import views
-
+'''
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
+'''
+urlpatterns=[
+url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+url(r'^$', views.post_list, name='post_list'),
+url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+url(r'^post/new/$', views.post_new, name='post_new'),
+url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+
+url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+]
+
+'''
+path(r'^$', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path(r'post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    path('/post/<pk>/remove/', views.post_remove, name='post_remove')
+]'''
